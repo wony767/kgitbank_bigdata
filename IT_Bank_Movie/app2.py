e91734c39b976e09c4b2e41383dacc51c5eaf72e
shimseonjo <shimseonjo@gmail.com>
2019-04-06 20:23:34 +0900

    영화상세페이지 워드클라우드 수정

--- IT_Bank_Movie/app2.py
+++ IT_Bank_Movie/app2.py
@@ -4,6 +4,38 @@ from bs4 import BeautifulSoup
 import urllib.request
 from selenium import webdriver as wd
 import time,re
+#영화 코드넣기
+import sys
+import urllib.request,json, time
+import pandas as pd
+import numpy as np
+import re
+from soynlp.tokenizer import RegexTokenizer
+from soynlp.noun import LRNounExtractor
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from PIL import Image
+
+tmrvl=[]
+url="https://movie.naver.com/movie/running/current.nhn"
+response = urllib.request.urlopen(url)
+soup=BeautifulSoup(response,'html.parser')
+table=soup.select('dt.tit a')
+for result3 in table:
+        mtitle=str(result3.string)
+        mcode=str(result3.attrs['href'])
+        i = str(re.findall('\d+', mcode)[0])
+        tmcode=tuple([i])
+        tmtitle=tuple([mtitle])
+        tmrvl.append(tmtitle+tmcode)
+conn=pymysql.connect(host='127.0.0.1',user='root',password='qwer1234',db='movie',charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+c=conn.cursor()
+#마리아 db에 넣을댸는 ??가아니고 %s로 써야ㅚㅁ
+sql="INSERT IGNORE INTO test(title,codem) VALUES(%s,%s)"
+c.executemany(sql, tmrvl)
+conn.commit()
+conn.close()
+#-------
 
 #flask는 웹, request는 값전달,render_template는 html띄우기위함
 
@@ -86,11 +118,9 @@ def searchmovie():
 
 
 #영화상세
-@app.route('/movie_detail/<m_no>')#주소임
-def detail(m_no):
-
+@app.route('/movie_detail/<m_no>/<current_movie_title>')#주소임
+def detail(m_no,current_movie_title):
     
-
     conn=pymysql.connect(host='127.0.0.1',
     user='root',
     password='qwer1234',
@@ -99,8 +129,8 @@ def detail(m_no):
     cursorclass=pymysql.cursors.DictCursor)
     try:
         with conn.cursor() as cursor:
-            sql='select * from current_movie where m_no= %s;'
-            cursor.execute(sql,(m_no))
+            sql='select * from current_movie c inner join test t on c.current_movie_title = t.title where current_movie_title = %s;'
+            cursor.execute(sql,(current_movie_title))
             result=cursor.fetchone() #하나만 가져올떄
 
             sql='select * from board where m_no= %s;'
@@ -108,6 +138,57 @@ def detail(m_no):
             board=cursor.fetchall()
     finally:
         conn.close()
+        
+    tmrvl=[]
+    movieName = result['codem']
+
+    for page in range(1,200):
+        url="https://movie.naver.com/movie/bi/mi/review.nhn?code="+str(movieName)+"&page="+str(page)
+        response = urllib.request.urlopen(url)
+
+        soup=BeautifulSoup(response,'html.parser')
+        table=soup.select('ul.rvw_list_area li a')
+        for result3 in table:
+            mrv=str(result3.string)
+            tmrv=tuple([mrv])
+            tmrvl.append(tmrv)
+            #tmrv1=str(tmrv)
+            #f.write(tmrv1)
+    df=pd.DataFrame(tmrvl)
+
+    def preprocessing(text):
+        # 개행문자 제거
+        text = re.sub('\\\\n', ' ', text)
+        return text
+    
+    tokenizer = RegexTokenizer()
+    stopwords_kr = ['하지만', '그리고', '그런데', '저는','제가',
+                    '그럼', '이런', '저런', '합니다',
+                    '많은', '많이', '정말', '너무','[',']','것으로','했습니다','했다'] 
+
+    sentences = df[0].apply(preprocessing)
+
+    # soynlp로 명사 추출하기
+    noun_extractor = LRNounExtractor(verbose=True)
+    noun_extractor.train(sentences)
+    nouns = noun_extractor.extract()
+   
+    # 이미지 파일위에 출력하기
+    img = Image.open('IT_Bank_Movie/static/wordcloud/apple.png')
+    img_array=np.array(img)
+
+    wordcloud = WordCloud( font_path = '/Library/Fonts/NanumBarunGothic.ttf', 
+                        stopwords = stopwords_kr,
+                        background_color = 'white', 
+                        mask=img_array,
+                        width = 800, height = 600).generate(' '.join(nouns))
+    plt.figure(figsize = (15 , 10))
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    #plt.show()  
+    url1="IT_Bank_Movie/static/wordcloud/" + current_movie_title + ".png"
+    wordcloud.to_file(url1)
+
     return render_template('movie_detail.html', movieInfo=result, board=board)
 
 #상영예정영화상세
