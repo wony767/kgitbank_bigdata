4c2e87e1e63412261fab35683e9042ad7db72052
seonjo <shimseonjo@gmail.com>
2019-04-07 11:12:23 +0900

    최종2

--- IT_Bank_Movie/app2.py
+++ IT_Bank_Movie/app2.py
@@ -133,63 +133,68 @@ def detail(m_no,current_movie_title):
             cursor.execute(sql,(current_movie_title))
             result=cursor.fetchone() #하나만 가져올떄
 
+            sql='select * from current_movie where current_movie_title = %s;'
+            cursor.execute(sql,(current_movie_title))
+            result1=cursor.fetchone() #하나만 가져올떄
+
             sql='select * from board where m_no= %s;'
             cursor.execute(sql,(m_no))
             board=cursor.fetchall()
     finally:
         conn.close()
+    if result is not None:    
+        tmrvl=[]
+        movieName = result['codem']
+
+        for page in range(1,200):
+            url="https://movie.naver.com/movie/bi/mi/review.nhn?code="+str(movieName)+"&page="+str(page)
+            response = urllib.request.urlopen(url)
+
+            soup=BeautifulSoup(response,'html.parser')
+            table=soup.select('ul.rvw_list_area li a')
+            for result3 in table:
+                mrv=str(result3.string)
+                tmrv=tuple([mrv])
+                tmrvl.append(tmrv)
+                #tmrv1=str(tmrv)
+                #f.write(tmrv1)
+        df=pd.DataFrame(tmrvl)
+
+        def preprocessing(text):
+            # 개행문자 제거
+            text = re.sub('\\\\n', ' ', text)
+            return text
         
-    tmrvl=[]
-    movieName = result['codem']
+        tokenizer = RegexTokenizer()
+        stopwords_kr = ['하지만', '그리고', '그런데', '저는','제가',
+                        '그럼', '이런', '저런', '합니다',
+                        '많은', '많이', '정말', '너무','[',']','것으로','했습니다','했다'] 
 
-    for page in range(1,200):
-        url="https://movie.naver.com/movie/bi/mi/review.nhn?code="+str(movieName)+"&page="+str(page)
-        response = urllib.request.urlopen(url)
+        sentences = df[0].apply(preprocessing)
 
-        soup=BeautifulSoup(response,'html.parser')
-        table=soup.select('ul.rvw_list_area li a')
-        for result3 in table:
-            mrv=str(result3.string)
-            tmrv=tuple([mrv])
-            tmrvl.append(tmrv)
-            #tmrv1=str(tmrv)
-            #f.write(tmrv1)
-    df=pd.DataFrame(tmrvl)
-
-    def preprocessing(text):
-        # 개행문자 제거
-        text = re.sub('\\\\n', ' ', text)
-        return text
+        # soynlp로 명사 추출하기
+        noun_extractor = LRNounExtractor(verbose=True)
+        noun_extractor.train(sentences)
+        nouns = noun_extractor.extract()
     
-    tokenizer = RegexTokenizer()
-    stopwords_kr = ['하지만', '그리고', '그런데', '저는','제가',
-                    '그럼', '이런', '저런', '합니다',
-                    '많은', '많이', '정말', '너무','[',']','것으로','했습니다','했다'] 
+        # 이미지 파일위에 출력하기
+        img = Image.open('IT_Bank_Movie/static/img/cloud.png')
+        img_array=np.array(img)
 
-    sentences = df[0].apply(preprocessing)
+        wordcloud = WordCloud( font_path = '/Library/Fonts/NanumBarunGothic.ttf', 
+                            stopwords = stopwords_kr,
+                            background_color = 'white', 
+                            mask=img_array,
+                            width = 800, height = 600).generate(' '.join(nouns))
+        plt.figure(figsize = (15 , 10))
+        plt.imshow(wordcloud)
+        plt.axis("off")
+        #plt.show()  
+        url1="IT_Bank_Movie/static/wordcloud/" + current_movie_title + ".png"
+        wordcloud.to_file(url1)
+    
 
-    # soynlp로 명사 추출하기
-    noun_extractor = LRNounExtractor(verbose=True)
-    noun_extractor.train(sentences)
-    nouns = noun_extractor.extract()
-   
-    # 이미지 파일위에 출력하기
-    img = Image.open('IT_Bank_Movie/static/img/cloud.png')
-    img_array=np.array(img)
-
-    wordcloud = WordCloud( font_path = '/Library/Fonts/NanumBarunGothic.ttf', 
-                        stopwords = stopwords_kr,
-                        background_color = 'white', 
-                        mask=img_array,
-                        width = 800, height = 600).generate(' '.join(nouns))
-    plt.figure(figsize = (15 , 10))
-    plt.imshow(wordcloud)
-    plt.axis("off")
-    #plt.show()  
-    url1="IT_Bank_Movie/static/wordcloud/" + current_movie_title + ".png"
-    wordcloud.to_file(url1)
-
-    return render_template('movie_detail.html', movieInfo=result, board=board)
+    return render_template('movie_detail.html', wordInfo=result, board=board, movieInfo=result1)
 
 #상영예정영화상세
 @app.route('/show_movie_detail/<m_no>')#주소임
